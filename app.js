db22f7cd8b096a804da0b169f58d3a35ee2d7ce3
Allen Stevens <allen.stevens@students.dominican.edu>
2024-03-20 15:45:21 -0700

    styled page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // Require Libraries
 const express = require('express');
 const Tenor = require("tenorjs").client({
@@ -9,6 +10,7 @@ const Tenor = require("tenorjs").client({
 
 // App Setup
 const app = express();
+app.use(express.static('public'));
 
 // Middleware
 // Allow Express (our web framework) to render HTML templates and send them back to the client using a new function
