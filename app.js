0e2bb6a4a99953c596b536cc862a5362b96bc1ab
Allen Stevens <allen.stevens@students.dominican.edu>
2024-03-20 15:25:11 -0700

    added search form

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 // Routes
+// example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
-  })
+    console.log(req.query); // => "{ term: 'hey' }"
+    res.render('home');
+});
 
   app.get('/greetings/:name', (req, res) => {
     // grab the name from the path provided
